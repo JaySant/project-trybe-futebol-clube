2f70d55cd0e86071b8041e6134fdc3fa4f7c8251
JaySant <jaysant15@gmail.com>
2023-01-18 16:08:02 -0300

    Requisito tem logica para o req 19, 20, 21

--- app/backend/src/services/MatcheService.ts
+++ app/backend/src/services/MatcheService.ts
@@ -4,12 +4,23 @@ import MatchesModel from '../database/models/MatchesModel';
 export default class MatchesService {
   public matches = MatchesModel;
 
-  public async findMatches(): Promise<MatchesModel[]> {
+  public async findMatches(): Promise<MatchesModel[] | undefined> {
     const getAllMatches = await this.matches.findAll({
       include: [
         { model: TeamsModel, as: 'teamHome', attributes: { exclude: ['id'] } },
         { model: TeamsModel, as: 'teamAway', attributes: { exclude: ['id'] } },
       ] });
     return getAllMatches;
+  }
+
+  public async findProgress(query: boolean): Promise<MatchesModel[] | undefined> {
+    // console.log(query);
+    const findAllMatches = await this.matches.findAll({
+      where: { inProgress: query },
+      include: [
+        { model: TeamsModel, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: TeamsModel, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ] });
+    return findAllMatches;
   }
 }
