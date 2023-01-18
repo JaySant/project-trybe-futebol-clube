af73941515be4ebb40e899d4ead7b159760ee018
JaySant <jaysant15@gmail.com>
2023-01-18 01:31:49 -0300

    ARRUMAR CONFIG

--- app/backend/src/controllers/TeamControllers.ts
+++ app/backend/src/controllers/TeamControllers.ts
@@ -1,11 +1,10 @@
-import { Request, Response } from 'express';
-import TeamsService from '../services/TeamService';
+// import { Request, Response } from 'express';
 
-export default class TeamsControllers {
-  public teamService = new TeamsService();
+// export default class TeamsControllers {
+//   public teamService = new TeamsService();
 
-  async getTeams(_req: Request, res: Response) {
-    const getAll = await this.teamService.findTeam();
-    return res.status(200).json(getAll)
-   }
-}
+//   async getTeams(_req: Request, res: Response) {
+//     const getAll = await this.teamService.findTeam();
+//     return res.status(200).json(getAll);
+//   }
+// }
